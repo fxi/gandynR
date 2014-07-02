141cffe40a544052badf1a5a4ada5961effa78bf
fxi <moser.frederic@gmail.com>
2014-07-02 21:26:44 +0200

    add sample config file

---
+++ gandynR-config.example.R
@@ -0,0 +1,26 @@
+
+
+######################################
+#
+# Exemple config. Please rename this file
+# to gandynR-config.R
+#
+######################################
+
+library('XMLRPC')
+
+
+# # gandi.net api key
+aKey<-'yourApiKey03oinr3508fn5hohoho'
+
+# # domain name
+dName<-'yourDomainName.com'
+
+# # user id (for ssh connection)
+userId = 'RandySmith'
+
+# # ssh port (keep empty for 22)
+sshPort = 12123
+
+# # log directory : where to write output 
+logDir<-'~/'
